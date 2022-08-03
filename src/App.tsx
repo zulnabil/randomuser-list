c46c505d648dacd1372fa0779e1d7d42d710eaa5
Zulnabil Alhadad <zulnabil.22@gmail.com>
2022-08-03 11:54:35 +0700

    feat(page): init dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import DashboardPage from "pages/dashboard.page"
 import "./common/styles/index.scss"
 
 function App() {
   return (
     <div className="App">
+      <DashboardPage />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/dashboard.page.tsx
@@ -0,0 +1,11 @@
+import LayoutComponent from "layouts/layout.component"
+
+const DashboardPage = () => {
+  return (
+    <LayoutComponent>
+      <div className="ui-page">Dashboard page</div>
+    </LayoutComponent>
+  )
+}
+
+export default DashboardPage
